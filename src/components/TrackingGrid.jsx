24b68d07751c97b5ee8b90ce34cf328193575fd8
dkersten <dkersten1994@gmail.com>
2023-05-16 20:24:06 -0400

    continuing work on activity tracking chart

--- src/components/TrackingGrid.jsx
+++ src/components/TrackingGrid.jsx
@@ -1,70 +1,32 @@
 import '../styling/tracking-grid.scss'
+import Calendar from 'react-github-contribution-calendar'
+
+import activityChartData from '../tempData/activityChartData';
 
 const TrackingGrid = () => {
 
-// Want to track: 
-// 1. exercise
-// 2. range session
-// 3. short game practice
-// 4. putting practice
-// 5. round played
+  const date = new Date()
+  const day = date.getDate()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear()
+  const until = `${year}-${month}-${day}`
 
-  const buildTrackerRow = () => {
-    const rows = []
-    for (let i = 0; i < 7; i++) {
-      rows.push(<tr class="tracker-row">{buildMainTrackerColumns()}</tr>)
-    }
-
-    return rows
-  }
-
-  // temporary function to simulate activity. Will need to be removed when actual data exists
-  const activityGenerator = () => {
-    const min = 0;
-    const max = 5;
-    const randNum = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    return `activity-level-${randNum}`
-  }
-
-  const buildMainTrackerColumns = () => {
-    const cells = []
-
-    for (let i = 0; i < 52; i++) {
-      cells.push(<td className={`daily-tracking-cell daily-tracking-cell-${activityGenerator()}`}></td>)
-    }
-
-    return cells
-  }
+  const panelColors = ['#f2f2f2ec','#a0dbeb', '#61c3de', '#4e9cb2']
 
   return (
-    <section className="daily-tracking" >
-      <h2>Practice Tracking</h2>
-    <table className='daily-tracking__grid'>
-      <thead>
-        {/* these will need to be dynamic based on the number of days in the month and what day of the week the year started on */}
-        <tr>
-          <th className="daily-tracking__header-cell"><span className="daily-tracking__header-day">Day</span></th>
-          <th className="daily-tracking__header-cell" colspan="4">Jan</th>
-          <th className="daily-tracking__header-cell" colspan="4">Feb</th>
-          <th className="daily-tracking__header-cell" colspan="4">Mar</th>
-          <th className="daily-tracking__header-cell" colspan="4">Apr</th>
-          <th className="daily-tracking__header-cell" colspan="4">May</th>
-          <th className="daily-tracking__header-cell" colspan="4">Jun</th>
-          <th className="daily-tracking__header-cell" colspan="4">Jul</th>
-          <th className="daily-tracking__header-cell" colspan="4">Aug</th>
-          <th className="daily-tracking__header-cell" colspan="4">Sep</th>
-          <th className="daily-tracking__header-cell" colspan="4">Oct</th>
-          <th className="daily-tracking__header-cell" colspan="4">Nov</th>
-          <th className="daily-tracking__header-cell" colspan="4">Dec</th>
-        </tr>
-      </thead>
-      <tbody>
-        {buildTrackerRow()}
-      </tbody>
-    </table>
+    <section className="activity-tracking">
+      <h2>Activity Tracking</h2>
+      <Calendar values={activityChartData} until={until} panelColors={panelColors} />
+      <div className="activity-tracking__key">
+        <span className="activity-tracking__level-key">Less</span>
+        <div className="activity-tracking__level activity-tracking__level-1"></div>
+        <div className="activity-tracking__level activity-tracking__level-2"></div>
+        <div className="activity-tracking__level activity-tracking__level-3"></div>
+        <div className="activity-tracking__level activity-tracking__level-4"></div>
+        <span className="activity-tracking__level-key">More</span>
+      </div>
     </section>
-  );
+  )
 };
 
 export default TrackingGrid;
