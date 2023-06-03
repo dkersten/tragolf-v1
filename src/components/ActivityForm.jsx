7d8eada9350546af5671c3af70c2dd94e00dae19
dkersten <dkersten1994@gmail.com>
2023-06-03 14:27:37 -0400

    working on activity form

--- src/components/ActivityForm.jsx
+++ src/components/ActivityForm.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
-import { Button, Modal, DatePicker } from 'antd';
+import { Button, Modal, DatePicker, Select } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
 const ActivityForm = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [puttingMatStats, setPuttingMatStats] = useState(false);
 
+  //Activity Options
+  const activityOptions = [
+    {label: "Round Played", value: "roundPlayed"},
+    {label: "Short Game Practice", value: "shortGamePractice"},
+    {label: "Range Session", value: "rangeSession"},
+    {label: "Putting Practice", value: "puttingPractice"},
+    {label: "Putting Mat Practice", value: "puttingMatPractice"},
+    {label: "Exercise - Mobility and Strength", value: "exerciseMobilityAndStrength"},
+    {label: "Exercise - Strength Training", value: "exerciseStrengthTraining"},
+    {label: "Exercise - Cardio", value: "exerciseCardio"},
+    {label: "Simulator - Round Played", value: "simulatorRoundPlayed"},
+    {label: "Simulator - Range Session", value: "simulatorRangeSession"},
+    {label: "Simulator - Short Game Practice", value: "simulatorShortGamePractice"},
+  ]
+
+  //   form controls
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -17,18 +34,38 @@ const ActivityForm = () => {
     setIsModalOpen(false);
   };
 
-  const onChange = (date, dateString) => {
+  const onDateChange = (date, dateString) => {
     console.log(date, dateString);
   };
+
+  const handleActivityChange = () => {
+    console.log("activity change")
+  }
 
   return (
     <>
       <Button type="text" onClick={showModal}>
         <PlusOutlined />
       </Button>
-      <Modal title="Add Activities" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <Modal title="Add Activities" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} okText="Submit">
         <form>
-            <DatePicker onChange={onChange} />
+            <div className='activity-form__input'>
+                <label>Date:</label>
+                <DatePicker onChange={onDateChange} />
+            </div>
+            <div className='activity-form__input'>
+                <label>Type:</label>
+                <Select
+                mode="multiple"
+                allowClear
+                style={{
+                    width: '100%',
+                }}
+                placeholder="Please select"
+                onChange={handleActivityChange}
+                options={activityOptions}
+                />
+            </div>
         </form>
       </Modal>
     </>
