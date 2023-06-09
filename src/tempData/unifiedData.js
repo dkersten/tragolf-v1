081fe1c37233b31a39fec2d6aa80cab09143f3bf
dkersten <dkersten1994@gmail.com>
2023-06-09 00:01:45 -0400

    updated unified data

--- src/tempData/unifiedData.js
+++ src/tempData/unifiedData.js
@@ -1,65 +1,346 @@
 const unifiedData = {
     activityTotals: {
-        roundPlayed: 0,
-        shortGamePractice: 0,
-        rangeSession: 0,
-        puttingPractice: 0,
-        puttingMatPractice: 0,
-        exerciseMobilityAndStrength: 0,
-        exerciseStrengthTraining: 0,
-        exerciseCardio: 0,
+        roundPlayed: 20,
+        shortGamePractice: 12,
+        rangeSession: 10,
+        puttingPractice: 9,
+        puttingMatPractice: 27,
+        exerciseMobilityAndStrength: 4,
+        exerciseStrengthTraining: 11,
+        exerciseCardio: 1,
         simulatorRoundPlayed: 0,
         simulatorRangeSession: 0,
         simulatorShortGamePractice: 0,
         golfLesson: 0
     },
     activityDailies: {
-        "2023-04-18": {
+        "2023-06-07": {
+            numActivities: 1,
+            activities: [
+                "exerciseStrengthTraining"
+            ]
+        },
+        "2023-06-06": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-06-05": {
+            numActivities: 1,
+            activities: [
+                "exerciseCardio"
+            ]
+        },
+        "2023-06-04": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-06-03": {
+            numActivities: 2,
+            activities: [
+                "shortGamePractice",
+                "puttingPractice"
+            ]
+        },
+        "2023-06-02": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-31": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-29": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-27": {
+            numActivities: 2,
+            activities: [
+                "shortGamePractice",
+                "puttingPractice"
+            ]
+        },
+        "2023-05-26": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-25": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-24": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-23": {
             numActivities: 1,
             activities: [
                 "rangeSession"
             ]
         },
-        "2023-04-18": {
+        "2023-05-22": {
+            numActivities: 2,
+            activities: [
+                "shortGamePractice",
+                "puttingPractice"
+            ]
+        },
+        "2023-05-20": {
+            numActivities: 2,
+            activities: [
+                "shortGamePractice",
+                "puttingPractice"
+            ]
+        },
+        "2023-05-19": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-16": {
+            numActivities: 2,
+            activities: [
+                "shortGamePractice",
+                "puttingPractice"
+            ]
+        },
+        "2023-05-15": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-14": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-13": {
+            numActivities: 2,
+            activities: [
+                "shortGamePractice",
+                "puttingPractice"
+            ]
+        },
+        "2023-05-12": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-11": {
+            numActivities: 1,
+            activities: [
+                "shortGamePractice"
+            ]
+        },
+        "2023-05-10": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-06": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-05": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-04": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-05-03": {
+            numActivities: 1,
+            activities: [
+                "exerciseStrengthTraining"
+            ]
+        },
+        "2023-05-01": {
+            numActivities: 1,
+            activities: [
+                "exerciseStrengthTraining"
+            ]
+        },
+        "2023-04-30": {
             numActivities: 1,
             activities: [
                 "rangeSession"
             ]
         },
-        "2023-04-18": {
-            numActivities: 1,
+        "2023-04-29": {
+            numActivities: 3,
             activities: [
-                "rangeSession"
+                "shortGamePractice",
+                "puttingPractice",
+                "exerciseStrengthTraining"
             ]
         },
-        "2023-04-18": {
-            numActivities: 1,
+        "2023-04-27": {
+            numActivities: 2,
             activities: [
-                "rangeSession"
+                "shortGamePractice",
+                "exerciseStrengthTraining"
             ]
         },
-        "2023-04-18": {
+        "2023-04-26": {
             numActivities: 1,
             activities: [
-                "rangeSession"
+                "puttingMatPractice"
             ]
         },
-        "2023-04-18": {
+        "2023-04-25": {
             numActivities: 1,
             activities: [
-                "rangeSession"
+                "puttingMatPractice"
             ]
         },
-        "2023-04-18": {
-            numActivities: 1,
+        "2023-04-24": {
+            numActivities: 2,
             activities: [
-                "rangeSession"
+                "puttingMatPractice",
+                "exerciseStrengthTraining"
             ]
         },
-        "2023-04-18": {
+        "2023-04-23": {
+            numActivities: 2,
+            activities: [
+                "puttingMatPractice",
+                "exerciseStrengthTraining"
+            ]
+        },
+        "2023-04-22": {
             numActivities: 1,
             activities: [
-                "rangeSession"
+                "roundPlayed"
+            ]
+        },
+        "2023-04-21": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-04-20": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-04-17": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-16": {
+            numActivities: 1,
+            activities: [
+                "roundPlayed"
+            ]
+        },
+        "2023-04-15": {
+            numActivities: 2,
+            activities: [
+                "rangeSession",
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-13": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-12": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-11": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-10": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-09": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-08": {
+            numActivities: 2,
+            activities: [
+                "puttingMatPractice",
+                "exerciseStrengthTraining"
+            ]
+        },
+        "2023-04-07": {
+            numActivities: 4,
+            activities: [
+                "rangeSession",
+                "shortGamePractice",
+                "puttingPractice",
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-06": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-05": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-03": {
+            numActivities: 2,
+            activities: [
+                "rangeSession",
+                "puttingMatPractice"
+            ]
+        },
+        "2023-04-02": {
+            numActivities: 1,
+            activities: [
+                "puttingMatPractice"
             ]
         },
         "2023-04-01": {
